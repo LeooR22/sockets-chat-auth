007aeea87bc881e63d70004886a73c2192c5c381
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-06-16 23:33:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chat app - Sockets - Auth
+
+Recuerden que deben de ejecutar `npm install` para reconstruir los módulos de Node.
+
+La app cuenta con chat privado y global
---
+++ models/chat-mensajes.js
@@ -0,0 +1,37 @@
+class Mensajes {
+  constructor(uid, nombre, mensaje) {
+    this.uid = uid;
+    this.nombre = nombre;
+    this.mensaje = mensaje;
+  }
+}
+
+class ChatMensajes {
+  constructor() {
+    this.mensajes = [];
+    this.usuarios = {};
+  }
+
+  get ultimos10() {
+    this.mensajes = this.mensajes.splice(0, 10);
+    return this.mensajes;
+  }
+
+  get usuariosArr() {
+    return Object.values(this.usuarios); // [ {}, {}, {}]
+  }
+
+  enviarMensaje(uid, nombre, mensajes) {
+    this.mensajes.unshift(new Mensajes(uid, nombre, mensajes));
+  }
+
+  conectarUsuario(usuario) {
+    this.usuarios[usuario.id] = usuario;
+  }
+
+  desconectarUsuario(id) {
+    delete this.usuarios[id];
+  }
+}
+
+module.exports = ChatMensajes;
